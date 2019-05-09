691f7eb722c354a202f6220181b7bc5735cd09ef
rehamsamy <reham.samy2012@yahoo.com>
2019-05-09 10:56:20 -0700

    new

--- app/src/main/java/com/example/mohamed/capstone/UsersActivity.java
+++ app/src/main/java/com/example/mohamed/capstone/UsersActivity.java
@@ -2,6 +2,7 @@ package com.example.mohamed.capstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
@@ -29,8 +30,9 @@ public class UsersActivity extends AppCompatActivity implements UserAdapter.OnCl
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
-       // users=new ArrayList<>();
+        users=new ArrayList<>();
         recyclerView=(RecyclerView) findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
        // adapter=new UserAdapter(this,users,this);
 
         firebaseDatabase=FirebaseDatabase.getInstance();
@@ -51,13 +53,17 @@ public class UsersActivity extends AppCompatActivity implements UserAdapter.OnCl
                         String image=xData.child("image").getValue().toString();
                         String status=xData.child("status").getValue().toString();
                         users=new ArrayList<>();
-                        User user1=new User(name,image,status);
-                        users.add(user1);
+                        User user=new User(name,image,status);
+                        users.add(user);
                        //Log.v(TAG,"userValue"+users.get(1));
+                       Log.v(TAG,"userValue"+users.size());
+
                     }
-                    Log.v(TAG,"userValue"+users.size());
                     adapter=new UserAdapter(UsersActivity.this,users,UsersActivity.this);
                     recyclerView.setAdapter(adapter);
+                    Log.v(TAG,"userValue"+users.size());
+
+
                     Log.v(TAG,"userValue"+users.size());
                 }
 
